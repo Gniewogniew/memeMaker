90337b969851dfb95eaea93871e556c32b4ea42f
Gniewogniew <jakubbajer83@gmail.com>
2017-08-30 20:17:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,81 +1,63 @@
+function textChangeListener(evt) {
+    var id = evt.target.id;
+    var text = evt.target.value;
 
-function textChangeListener (evt) {
-      var id = evt.target.id;
-      var text = evt.target.value;
-      
-      if (id == "textOnTop") {
+    if (id == "textOnTop") {
         window.topLineText = text;
-      } else {
+    } else {
         window.bottomLineText = text;
-      }
-      
-      drawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
     }
+    drawMeme(window.imageSrc, window.topLineText, window.bottomLineText);
+}
 
-    function drawMeme(image, topLine, bottomLine) {
-
-      var canvas = document.querySelector('canvas');
-      var ctx = canvas.getContext("2d");
-      if (image != null)
+function drawMeme(image, topLine, bottomLine) {
+    if (image != null) {
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-      ctx.clearRect (0, 0,  canvas.width, canvas.height);
-      
-      if (image != null)
-	ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
-      
-
-      ctx.font = '50pt Impact';
-      ctx.textAlign = 'center';
-      ctx.strokeStyle = 'black';
-      ctx.lineWidth = 4;
-      ctx.fillStyle = 'white';
-      
-      if (topLine != null) {
+    }
+    if (topLine != null) {
         ctx.fillText(topLine, canvas.width / 2, 80);
         ctx.strokeText(topLine, canvas.width / 2, 80);
-      }
-      
-      if (bottomLine != null) {
+    }
+    if (bottomLine != null) {
         ctx.fillText(bottomLine, canvas.width / 2, canvas.height - 20);
         ctx.strokeText(bottomLine, canvas.width / 2, canvas.height - 20);
-      }
-    }
-
-function saveFile(event) {
-	var display = confirm ("Czy chcesz zapisać ten obraz?")
-	if (display == true) {
-    var data = canvas.toDataURL();
-    download.href = data;
-    }else{
-     event.preventDefault();
     }
 }
- 
-  function loadImage(event) {
-  	var canvasWidth = 700;
-      var canvasHeight = 600;
-  	var ctx = canvas.getContext("2d");
-    var reader = new FileReader();
-    reader.onload = function(event){
-      var image = new Image();
-        image.onload = function(){
-          window.imageSrc = this;
-          drawMeme(window.imageSrc, null, null);
+
+function saveFile(event) {
+    var display = confirm("Czy chcesz zapisać ten obraz?")
+    if (display == true) {
+        var data = canvas.toDataURL();
+        download.href = data;
+    } else {
+        event.preventDefault();
+    }
+}
+
+function loadImage(event) {
+    reader.onload = function(event) {
+        var image = new Image();
+        image.onload = function() {
+            window.imageSrc = this;
+            drawMeme(window.imageSrc, null, null);
         }
         image.src = event.target.result;
-
     }
-    reader.readAsDataURL(event.target.files[0]);  
-  }
+    reader.readAsDataURL(event.target.files[0]);
+}
 
+var canvas = document.getElementById("MemeCanvas");
+var ctx = canvas.getContext("2d");
+var canvasWidth = 700;
+var canvasHeight = 600;
+var reader = new FileReader();
+ctx.font = "50pt Impact";
+ctx.textAlign = "center";
+ctx.strokeStyle = "black";
+ctx.lineWidth = 4;
+ctx.fillStyle = "white";
 
-    window.textOnTop = "";
-    window.textOnBottom = "";
-    var imageLoader = document.getElementById("fileInput");
-  	imageLoader.addEventListener("change", loadImage, false);
-    var download = document.getElementById("savebutton");
-  	download.addEventListener("click", saveFile, false);
-  	var input1 = document.getElementById("textOnTop");
-    var input2 = document.getElementById("textOnBottom");
-    input1.oninput = textChangeListener;
-    input2.oninput = textChangeListener;
+document.getElementById("fileInput").addEventListener("change", loadImage, false);
+document.getElementById("savebutton").addEventListener("click", saveFile, false);
+document.getElementById("textOnTop").addEventListener("keyup", textChangeListener, false);
+document.getElementById("textOnBottom").addEventListener("keyup", textChangeListener, false);
